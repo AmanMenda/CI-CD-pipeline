2d92ca96dd01301ed2e9b3b96eca40bcc967c852
Aman Menda <aman@LAPTOP-2362G6TO>
2024-07-06 20:12:22 +0100

    fix: remove target folder from repository

--- sample/target/debug/deps/tests-6a67d45d2b8d111c.d
+++
@@ -1,6 +0,0 @@
-/home/aman/CI-CD-pipeline/sample/target/debug/deps/tests-6a67d45d2b8d111c: tests/add_test.rs tests/../src/utils.rs
-
-/home/aman/CI-CD-pipeline/sample/target/debug/deps/tests-6a67d45d2b8d111c.d: tests/add_test.rs tests/../src/utils.rs
-
-tests/add_test.rs:
-tests/../src/utils.rs:
--- sample/target/debug/sample.d
+++
@@ -1,1 +0,0 @@
-/home/aman/CI-CD-pipeline/sample/target/debug/sample: /home/aman/CI-CD-pipeline/sample/src/main.rs /home/aman/CI-CD-pipeline/sample/src/utils.rs
