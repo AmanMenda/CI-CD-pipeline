ae3f9868b8137b16e1739946526686688a38c5f8
Aman Menda <aman.menda@epitech.eu>
2024-07-07 16:48:22 +0100

    chores: update env.exemple and launch_instance.sh

--- scripts/launch_instance.sh
+++ scripts/launch_instance.sh
@@ -1,3 +1,7 @@
+# If jenkins-container already exists, remove it and delete dangling images
+docker ps -q --filter "name=jenkins-container" | grep -q . && docker stop jenkins-container
+docker image prune -a
+
 docker build -t jenkins-image:jcasc -f Jenkins/image/Dockerfile Jenkins/image/
 
 # Ensure that environment variables are correctly loaded
@@ -5,8 +9,9 @@ docker build -t jenkins-image:jcasc -f Jenkins/image/Dockerfile Jenkins/image/
 # It's recommended to prefix the variables with `JENKINS_`
 # to facilitate filtering with `grep`.
 
+# TODO: Give the option to run in detached head
 docker run --rm \
     --name jenkins-container \
     --env-file .env \
-    -p 8081:8080 -d \
+    -p 8081:8080 \
     jenkins-image:jcasc
