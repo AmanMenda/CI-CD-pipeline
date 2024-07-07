6304fba3f718eb5ddcffd66de84a29c736d9dbda
Aman Menda <aman.menda@epitech.eu>
2024-07-07 01:07:08 +0100

    fix: remove target/

--- sample/target/debug/deps/sample-da592f32d0161144.d
+++
@@ -1,6 +0,0 @@
-/home/aman/CI-CD-pipeline/sample/target/debug/deps/sample-da592f32d0161144: src/main.rs src/utils.rs
-
-/home/aman/CI-CD-pipeline/sample/target/debug/deps/sample-da592f32d0161144.d: src/main.rs src/utils.rs
-
-src/main.rs:
-src/utils.rs:
