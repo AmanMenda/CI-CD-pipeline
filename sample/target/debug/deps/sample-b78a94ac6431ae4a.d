64837190dfa82afdc4e0727cc9e4b0d3f49d7d0a
Aman Menda <aman.menda@epitech.eu>
2024-07-07 01:05:46 +0100

    fix: mod utils name

---
+++ sample/target/debug/deps/sample-b78a94ac6431ae4a.d
@@ -0,0 +1,6 @@
+/home/aman/CI-CD-pipeline/sample/target/debug/deps/sample-b78a94ac6431ae4a: src/main.rs src/utils.rs
+
+/home/aman/CI-CD-pipeline/sample/target/debug/deps/sample-b78a94ac6431ae4a.d: src/main.rs src/utils.rs
+
+src/main.rs:
+src/utils.rs:
